e7cf8221e21d7e0f94ba1a7e0751ea74b486a9b2
권찬민 <gwonchanmin@gwonchanmin-ui-MacBookAir.local>
2023-05-19 15:41:10 +0900

    메세지함 수정

---
+++ runup/src/views/Questino&Answer/DetailQuestion.vue
@@ -0,0 +1,125 @@
+<template>
+    <v-container class="detailQue-container">
+        <v-row>
+            <v-card class="cotent-card">
+                <v-card-title>
+                    질문 제목: 금 캐는 방법에 대해 수업을 열어주셨으면 좋겠습니다.
+                </v-card-title>
+                <v-card-title>
+                    작성자: 맹구
+                </v-card-title>
+                <v-card-title>
+                    질문일: 2077-11-12
+                </v-card-title>
+                <v-card-title>
+                    질문 내용: 우리나라에서 금을 캐거나 사금을 채취할 수 있는 곳에 대해 알려주시고 도움을 주는 수업이 생기면 좋겠습니다!
+                </v-card-title>
+                <v-card-actions class="btn-group">
+                    <v-btn class="Que-btn-edit" :rounded="true" text to="/editQuestion"
+                        @click="editQuestionContent()">수정</v-btn>
+                    <v-btn class="Que-btn-del" :rounded="true" @click="delQuestionContent()">삭제</v-btn>
+                </v-card-actions>
+                <v-card-text>
+
+                </v-card-text>
+            </v-card>
+        </v-row>
+        <v-row>
+            <v-card class="cotent-card-comment">
+                <v-card-title>
+                    댓글
+                </v-card-title>
+                <v-row >
+                    <v-list-item-avatar class="comment-first">
+                        <v-img :src="$store.getters.getProfileImg"></v-img>
+                    </v-list-item-avatar>
+                    <v-col cols="2" >
+                        유리
+                    </v-col>
+                    <v-col cols="3">
+                        2099-11-11
+                    </v-col>
+                    <v-col cols="8" class="comment-first">
+                        유리가 깨지면 와장창
+                    </v-col>
+                    <v-card-actions class="btn-group">
+                        <v-btn class="Que-btn-edit" :rounded="true" text to="/editQuestion"
+                            @click="editQuestionContent()">수정</v-btn>
+                        <v-btn class="Que-btn-del" :rounded="true" @click="delQuestionContent()">삭제</v-btn>
+                    </v-card-actions>
+                </v-row>
+                <v-col cols="10" class="comment-write-control">
+                    <v-text-field class="comment-write" label="댓글 입력"></v-text-field>
+                </v-col>
+                <v-btn :rounded="true" class="comment-write-btn">등록</v-btn>
+                <v-container>
+                </v-container>
+            </v-card>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default{
+    name:'DetailQuestion',
+    data() {
+        return {
+
+        }
+    },
+    methods:{
+        editQuestionContent() {
+            alert("미완성")
+        },
+        delQuestionContent() {
+            alert("미완성")
+        }
+    },
+}
+</script>
+
+<style>
+.detailQue-container {
+    margin-top: 2%;
+}
+
+.cotent-card {
+    width: 100%;
+}
+
+.btn-group {
+    margin-left: 85%;
+}
+
+.Que-btn-edit {
+    background-color: wheat;
+}
+
+.Que-btn-del {
+    background-color: red !important;
+}
+
+/* 댓글 모음 */
+.cotent-card-comment {
+    margin-top: 2%;
+    width: 90%;
+    margin-left: 5%;
+}
+
+.cotent-card-comment {
+    max-width: 70%;
+}
+
+.comment-write-control {
+    display: inline-block;
+}
+
+.comment-write-btn {
+    background-color: cornsilk !important;
+    width: 10%;
+    margin-left: 3%;
+}
+
+.comment-first {
+    margin-left: 5%;
+}</style>
--- runup/src/views/Questino&Answer/QuestionBoard.vue
+++ runup/src/views/Questino&Answer/QuestionBoard.vue
@@ -1,92 +1,153 @@
 <template>
     <v-container class="Q-container">
+        <v-spacer></v-spacer>
+        <v-row justify="end">
+            <v-dialog v-model="dialog" persistent max-width="600px">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn color="cyan" class="mx-2" fab dark v-bind="attrs" v-on="on">
+                        <v-icon dark>
+                            mdi-pencil
+                        </v-icon>
+                    </v-btn>
+                </template>
+                <v-card>
+                    <v-card-title>
+                        <span class="text-h5">도움신청</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-container>
+                            <v-row>
+                                <v-col>
+                                    <v-radio-group row>
+                                        <v-radio label="도움요청" value="Option1">도움요청</v-radio>
+                                        <v-radio label="고민상담" value="Option2">고민상담</v-radio>
+                                    </v-radio-group>    
+                                </v-col>
+                                <v-col cols="12">
+                                    <v-text-field label="질문제목" v-model="WriteTitle" required></v-text-field>
+                                </v-col>
+                                <v-col cols="12">
+                                    <v-text-field v-model="WriteContent" label="질문내용" hint="Learner에게 강의할 내용을 간략하게 적어주세요"
+                                        required></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6">
+                                    <v-select v-model="choice" label="특기-대분류" :items="categories"
+                                        @change="fetchcategoryMedium"></v-select>
+                                </v-col>
+                                <v-col cols="3">
+                                    <v-select v-model="RunningMcategory" id="Running-Mcategory" label="특기-중분류"
+                                        :items="categoryMedium" @change="inputSelectVal"></v-select>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" text @click="createRunning(); dialog = false">
+                            등록
+                        </v-btn>
+                        <v-btn color="blue darken-1" text @click="dialog = false">
+                            취소
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-row>
         <v-row>
-            <v-card :loading="loading" class="Question-content">
-                <template slot="progress">
-                    <v-progress-linear color="deep-purple" height="10" indeterminate></v-progress-linear>
-                </template>
-
-                <v-img height="180" :rounded="true" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
-
-                <v-card-title>{{ this.$store.getters.getUserNickname }}</v-card-title>
-
-                <v-card-text>
-                    <v-row>
-                        <p>제목: </p>
-                    </v-row>
-                    <v-row>
-                        <p>작성자:</p>
-                    </v-row>
-                    <v-bottom-navigation :value="value" color="primary">
-                        <v-btn>
-                            <v-icon>mdi-history</v-icon>
-                        </v-btn>
-
-                        <v-btn>
-                            <v-icon>mdi-heart</v-icon>
-                        </v-btn>
-
-                        <v-btn>
-                            <v-icon>mdi-map-marker</v-icon>
-                        </v-btn>
-                    </v-bottom-navigation>
-                </v-card-text>
-            </v-card>
-            <v-card :loading="loading" class="Question-content">
-                <template slot="progress">
-                    <v-progress-linear color="deep-purple" height="10" indeterminate></v-progress-linear>
-                </template>
-
-                <v-img height="180" :rounded="true" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
-
-                <v-card-title>{{ this.$store.getters.getUserNickname }}</v-card-title>
-
-                <v-card-text>
-
-                </v-card-text>
-            </v-card>
-            <v-card :loading="loading" class="Question-content">
-                <template slot="progress">
-                    <v-progress-linear color="deep-purple" height="10" indeterminate></v-progress-linear>
-                </template>
-
-                <v-img height="180" :rounded="true" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
-
-                <v-card-title>{{ this.$store.getters.getUserNickname }}</v-card-title>
-
-                <v-card-text>
-
-                </v-card-text>
-            </v-card>
-            <v-card :loading="loading" class="Question-content">
-                <template slot="progress">
-                    <v-progress-linear color="deep-purple" height="10" indeterminate></v-progress-linear>
-                </template>
-
-                <v-img height="180" :rounded="true" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
-
-                <v-card-title>{{ this.$store.getters.getUserNickname }}</v-card-title>
-
-                <v-card-text>
-
-                </v-card-text>
-            </v-card>
+            <v-col v-for="(item, index) in runningList" :key="index" cols="4">
+                <v-card class="post-card-item" @click="GoDetail()">
+                    <v-img height="200" :rounded="true" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+                    <v-card-text>
+                        <div>{{ item.runningTitle }}</div>
+                        <div>{{ item.runningContent }}</div>
+                        <div>{{ item.runningCategoryMedium }}</div>
+                    </v-card-text>
+                </v-card>
+            </v-col>
         </v-row>
     </v-container>
 </template>
+  
+<script>
+// import axios from 'axios';
 
-<style>
+export default {
+    name: 'QuestionBoard',
+    data() {
+        return {
+            runningList: [],
+            dialog: false,
+            WriteTitle: '',
+            WriteContent: '',
+
+            //카테고리
+            categories: ['IT', '라이프스타일', '문제풀이', '기타'],
+            categoryMedium: [''],
+            choice: '',
+            RunningBcategory: '',
+            RunningMcategory: '',
+        };
+    },
+    mounted() {
+        this.fetchRunningList();
+    },
+    methods: {
+        fetchRunningList() {
+            var serverIP = '127.0.0.1',
+                serverPort = 8080,
+                pageUrl = 'runup/running/all';
+            this.$axios
+                .get(`http://${serverIP}:${serverPort}/${pageUrl}`)
+                .then((response) => {
+                    console.log(response.data);
+                    this.runningList = response.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        inputSelectVal(value) {
+            this.RunningMcategory = value;
+        },
+        fetchcategoryMedium() {
+            // console.log(this.choice)
+            var serverIP = '127.0.0.1',
+                serverPort = 8080,
+                pageUrl = 'runup/running/categorymedium';
+            this.$axios({
+                url: `http://${serverIP}:${serverPort}/${pageUrl}`,
+                method: "GET",
+                params: {
+                    categoryBig: this.choice
+                }
+            }).then(response => {
+                console.log(response)
+                const categoryMediumData = response.data.categoryMedium;
+                this.categoryMedium = ['전체'].concat(Object.values(categoryMediumData));
+            }).catch(error => {
+                console.log(error)
+            })
+        },
+        GoDetail() {
+            // let tmp = this;
+            // axios
+            // .get(tmp.baseUrl+"running",{
+            //     param: {
+            //         runningNum: this.runnningNum
+            //     }
+            // })
+            // .then((response) => {
+            //     console.log(response);
+            this.$router.push('/DetailQuestion');
+            // })
+        },
+    },
+};
+</script>
+  
+<style scoped>
 .Q-container {
-    display: inline-flex;
-    justify-content: space-between;
-    align-items: center;
     margin-top: 2%;
 }
-
-.Question-content {
-    display: inline-block;
-    width: 30%;
-    margin-left: 3%;
-    margin-bottom: 3%;
-}
 </style>
+  
